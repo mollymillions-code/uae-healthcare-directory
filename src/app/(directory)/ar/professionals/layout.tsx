0c2879bc0ab656d34cedcf38ef073189f7863309
sy.mu <sayan0151996@gmail.com>
2026-04-24 19:19:29 +0530

    Revert "Revert "fix(directory): restore ISR for bounded routes""
    This reverts commit 101e790cb4a4ef34ec1edc707a27f3ba78c830ed.

--- src/app/(directory)/ar/professionals/layout.tsx
+++
@@ -1,9 +0,0 @@
-export const dynamic = "force-dynamic";
-
-export default function ArabicProfessionalsLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return children;
-}
--- src/app/(directory)/directory/layout.tsx
+++
@@ -1,9 +0,0 @@
-export const dynamic = "force-dynamic";
-
-export default function DirectoryPagesLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return children;
-}
